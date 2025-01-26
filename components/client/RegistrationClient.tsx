c98f865bd9323f301617779a1d2a5472b18078f8
Nidhi Rao <157974518+nidraoo@users.noreply.github.com>
2025-01-26 22:41:23 +0530

    Update RegistrationClient.tsx
    updated name

--- components/client/RegistrationClient.tsx
+++ components/client/RegistrationClient.tsx
@@ -51,11 +51,11 @@ export const Registration = () => {
           <a
             href="#"
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >Spacify</a>
+          >HyperSpace</a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create an account for supplier
+                Create an account for Supplier
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                 <div>
@@ -91,7 +91,7 @@ export const Registration = () => {
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    placeholder="••••••••"
+                    placeholder="*******"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
                 </div>
