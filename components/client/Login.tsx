ae1610e1128f0ebf47c96436bd71a415bab75ced
Nidhi Rao <157974518+nidraoo@users.noreply.github.com>
2025-01-26 22:34:38 +0530

    Update Login.tsx

--- components/client/Login.tsx
+++ components/client/Login.tsx
@@ -78,7 +78,7 @@ export const Login = () => {
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    placeholder="••••••••"
+                    placeholder="*******"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
                 </div>
@@ -90,7 +90,7 @@ export const Login = () => {
                   Login for supplier
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Dont have an account yet?
+                  Don't have an account yet?
                   <Link
                     href="register"
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500"
