c4e5c25248ddd483a91e250697b070ccbefcbad7
nidraoo <05nidzzzzz@gmail.com>
2025-01-27 22:55:40 +0530

    changes in client pages

--- components/client/Login.tsx
+++ components/client/Login.tsx
@@ -87,13 +87,13 @@ export const Login = () => {
                   type="submit"
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Login for supplier
+                  Login
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
                   Don&apos;t have an account yet?
                   <Link
                     href="register"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    className="font-medium text-primary-600 underline dark:text-primary-500 ms-1"
                   >
                     Register here
                   </Link>
--- components/client/products/AddProduct.tsx
+++ components/client/products/AddProduct.tsx
@@ -7,21 +7,22 @@ import { toast } from "react-toastify";
 const AddProduct: FC = () => {
   const [productName, setProductName] = useState<string>("");
   const [price, setPrice] = useState<number>(0);
-  const [productDetails, setProductDetails] = useState<string>("");
+  const [spaceRequired, setSpaceRequired] = useState<number>(0);
+  const [type, setType] = useState<string>("Static");
 
   const router = useRouter();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
-    if (productName == "" || price <= -1 || productDetails == "") {
+    if (productName === "" || price <= -1 || spaceRequired <= 0) {
       toast.error("Please fill all fields");
       return;
     }
 
     const client = localStorage.getItem("client") || "";
 
-    if (client == "") {
+    if (client === "") {
       router.push("/client/login");
       return;
     }
@@ -30,8 +31,9 @@ const AddProduct: FC = () => {
     formdata.append("name", productName);
     formdata.append("price", price.toString());
     formdata.append("category", "any");
-    formdata.append("description", productDetails);
     formdata.append("client", client);
+    formdata.append("spaceRequired", spaceRequired.toString());
+    formdata.append("type", type);
 
     const res = await fetch(
       "https://mvr40.pythonanywhere.com/api/v1/addproduct",
@@ -41,12 +43,11 @@ const AddProduct: FC = () => {
       }
     );
 
-    if (res.status == 200) {
-      toast.success("Account created successfully");
-
+    if (res.status === 200) {
+      toast.success("Product added successfully");
       router.push("/client/products");
     } else {
-      toast.error("Error creating account");
+      toast.error("Error adding product");
     }
   };
 
@@ -58,7 +59,7 @@ const AddProduct: FC = () => {
 
   return (
     <form onSubmit={handleSubmit} className="m-10 rounded-3xl mx-32 p-10 bg-gray-900">
-      <h1 className="text-4xl font-bold text-center">Add Products</h1>
+      <h1 className="text-4xl font-bold text-center">Add Product</h1>
       <div className="my-6">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
           Product Name
@@ -74,7 +75,7 @@ const AddProduct: FC = () => {
       </div>
       <div className="mb-6">
         <label
-          htmlFor="password"
+          htmlFor="price"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
           Price
@@ -90,37 +91,37 @@ const AddProduct: FC = () => {
       </div>
       <div className="mb-6">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Product Details
+          Space Required (in square feet)
         </label>
-        <textarea
-          id="message"
-          rows={4}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Write your product details..."
-          value={productDetails}
-          onChange={(e) => setProductDetails(e.target.value)}
-        ></textarea>
+        <input
+          type="number"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+          value={spaceRequired}
+          onChange={(e) => setSpaceRequired(parseInt(e.target.value))}
+          placeholder="Space Required"
+        />
       </div>
 
-<div className="mb-9">
-<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-        distribucion Types
-      </label>
-      <select
-        id="countries"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      >
-        <option>Equal distribucion</option>
-        <option disabled>Custome distribucion</option>
-        <option disabled>Smart distribucion</option>
-      </select>
-</div>
+      <div className="mb-9">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Types
+        </label>
+        <select
+          id="types"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value="Static">Static</option>
+          <option value="Dynamic">Dynamic</option>
+        </select>
+      </div>
 
       <div className="w-full flex justify-center">
         <button
           type="submit"
           className="w-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onSubmit={handleSubmit}
         >
           Add Product
         </button>
@@ -130,3 +131,4 @@ const AddProduct: FC = () => {
 };
 
 export default AddProduct;
+
