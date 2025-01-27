d40a1dd400b89a03e571dcfeed2f7c1672b54bf6
Abhishek-2004-17 <117198301+Abhishek-2004-17@users.noreply.github.com>
2025-01-27 20:32:48 +0530

    Fixed login and registration for warehouse in components\warehouse\Login.tsx and components\warehouse\RegistrationWarehouse.tsx respectively.

--- components/warehouse/RegistrationWarehouse.tsx
+++ components/warehouse/RegistrationWarehouse.tsx
@@ -2,17 +2,46 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
-
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 export const RegistrationWarehouse = () => {
+
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-      router.push("/warehouse");
+        if(email == "" || name == "" || password == ""){
+            return;
+        }
+
+        const formdata = new FormData();
+        formdata.append("email", email);
+        formdata.append("name", name);
+        formdata.append("password", password);
+        
+        const res = await fetch("https://mvr40.pythonanywhere.com/api/v1/warecreate", {
+            method: "POST",
+            body: formdata
+        });
+            
+
+        if(res.status == 200){
+            toast.success("Account created successfully");
+            setEmail("");
+            setName("");
+            setPassword("");
+
+            router.push("/warehouse");
+        }else{
+            toast.error("Error creating account");
+        }
+        
     };
 
 
@@ -22,11 +51,11 @@ export const RegistrationWarehouse = () => {
           <a
             href="#"
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >Spacify</a>
+          >HyperSpace</a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create an account for warehouse owners
+                Create an account for Warehouse Owner
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                 <div>
@@ -35,8 +64,8 @@ export const RegistrationWarehouse = () => {
                   </label>
                   <input
                     type="email"
-                    name="email"
-                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                   />
@@ -47,29 +76,31 @@ export const RegistrationWarehouse = () => {
                   </label>
                   <input
                     type="text"
-                    name="name"
-                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="username"
                   />
                 </div>
+                
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Your Name
+                    Password
                   </label>
                   <input
-                    type="text"
-                    name="name"
-                    id="name"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="*******"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="username"
                   />
                 </div>
+                
                 <button
                   type="submit"
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Create an account for warehouse owners
+                  Create an account for Warehouse Owner
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already have an account?{" "}
