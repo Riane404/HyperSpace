9ed27bac68a432d31829d97fa52eb76e82bfa1c3
Abhishek-2004-17 <117198301+Abhishek-2004-17@users.noreply.github.com>
2025-01-28 14:34:48 +0530

    Reverting

--- components/client/Login.tsx
+++ components/client/Login.tsx
@@ -87,13 +87,13 @@ export const Login = () => {
                   type="submit"
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Login
+                  Login for supplier
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don&apos;t have an account yet?
                   <Link
                     href="register"
-                    className="font-medium text-primary-600 underline dark:text-primary-500 ms-1"
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
                     Register here
                   </Link>
